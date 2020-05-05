2238d59b21b8e6e182a4456941ae2f83ed0ed057
Nick Pilipchatin <50636011+brabus786@users.noreply.github.com>
2020-05-05 14:57:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const obj = {origin: '*'};
 app.use(cors(obj));
 app.use(express.json());
 app.use(router);
-app.listen(3001, () => {
+app.listen(5000, () => {
     console.log('server runs on the port 3001');
 });
 
